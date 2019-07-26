9e8e16c970f6dcb5cf0180fc193e0b33fa2a40eb
unknown <openfortrash@gmail.com>
2019-07-26 16:02:03 +0530

    fixed issue where empty local configs lists are pushed to db

--- banishing_bot/db_interface.py
+++ banishing_bot/db_interface.py
@@ -66,21 +66,20 @@ def send_and_receive_configs(reddit, unread_configs, subreddit_name_param):
     local_configs_str = receive_configs()
     if local_configs_str is not None:
         local_configs = eval(local_configs_str.replace('"', "'")) # gets list from text form in database (substitutes regular single quotes back in)
+        if unread_configs:
+            for u_config in unread_configs:
+                for l_config in local_configs:
+                    if u_config[subreddit_name_param] == l_config[subreddit_name_param]: # if same sub has sent another config
+                        local_configs[local_configs.index(l_config)] = u_config # updates local_config index accordingly
+                        unread_configs.remove(u_config) # removes item from unread configs
+            for u_config in unread_configs: # for remaining items in unread configs (brand-new configs)
+                local_configs.append(u_config) # adds brand-new configs to list of local configs to be sent
+        
+        banishing_bot = praw.models.Redditor(reddit, name='banishing_bot') # initializes banishing_bot's redditor class
+        local_configs = [l_config for l_config in local_configs if banishing_bot in reddit.subreddit(l_config[subreddit_name_param]).moderator()] # removes configs for subs in which bot is not a mod
+
+        send_configs(time.time(), str(local_configs).replace("'", '"')) # sends seconds since the epoch and local configs to be sent (substitutes double quotes for single quotes, for SQL)
     else:
         local_configs = []
-
-    if unread_configs:
-        for u_config in unread_configs:
-            for l_config in local_configs:
-                if u_config[subreddit_name_param] == l_config[subreddit_name_param]: # if same sub has sent another config
-                    local_configs[local_configs.index(l_config)] = u_config # updates local_config index accordingly
-                    unread_configs.remove(u_config) # removes item from unread configs
-        for u_config in unread_configs: # for remaining items in unread configs (brand-new configs)
-            local_configs.append(u_config) # adds brand-new configs to list of local configs to be sent
-    
-    banishing_bot = praw.models.Redditor(reddit, name='banishing_bot') # initializes banishing_bot's redditor class
-    local_configs = [l_config for l_config in local_configs if banishing_bot in reddit.subreddit(l_config[subreddit_name_param]).moderator()] # removes configs for subs in which bot is not a mod
-
-    send_configs(time.time(), str(local_configs).replace("'", '"')) # sends seconds since the epoch and local configs to be sent (substitutes double quotes for single quotes, for SQL)
 
     return local_configs
