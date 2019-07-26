258bddf80a192bc7cb1ed84a03c1201971029638
unknown <openfortrash@gmail.com>
2019-07-26 16:23:27 +0530

    fixed issue where db is not updated

--- banishing_bot/db_interface.py
+++ banishing_bot/db_interface.py
@@ -66,6 +66,10 @@ def send_and_receive_configs(reddit, unread_configs, subreddit_name_param):
     local_configs_str = receive_configs()
     if local_configs_str is not None:
         local_configs = eval(local_configs_str.replace('"', "'")) # gets list from text form in database (substitutes regular single quotes back in)
+    else:
+        local_configs = []
+
+    if local_configs:
         if unread_configs:
             for u_config in unread_configs:
                 for l_config in local_configs:
@@ -79,7 +83,5 @@ def send_and_receive_configs(reddit, unread_configs, subreddit_name_param):
         local_configs = [l_config for l_config in local_configs if banishing_bot in reddit.subreddit(l_config[subreddit_name_param]).moderator()] # removes configs for subs in which bot is not a mod
 
         send_configs(time.time(), str(local_configs).replace("'", '"')) # sends seconds since the epoch and local configs to be sent (substitutes double quotes for single quotes, for SQL)
-    else:
-        local_configs = []
 
     return local_configs
